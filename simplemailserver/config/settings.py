0ccb365247a3af839f44d317bf48a88907e9ebb5
Vasil Vangelovski <vasilvangelovski@Vasils-Mac-Pro.local>
2012-01-11 20:18:05 +0100

    Added some explanation in the settings file

--- simplemailserver/config/settings.py
+++ simplemailserver/config/settings.py
@@ -14,9 +14,18 @@ router_defaults = {'host': '.+'}
 
 template_config = {'dir': 'app', 'module': 'templates'}
 
+#If using hooks inside a django project we need to point to the django project settings file
+#in order to use django models
 import os
 os.environ['DJANGO_SETTINGS_MODULE']= 'djangoexample.settings'
 
+
+#Hooks are defined by destination host and destination address pattern
+#Each host can have different patterns, and each pattern has a hook defined that handles a message.
+#A hook is a callable that accepts three arguments message, address, host.
+#Any message to 123-123-123@localhost will be handled by handle_inportant
+#Any message to 123-123@localhost will be handled by handle_junk
+#any other address pattern including these will be forwarded to the relay
 hooks = {
     'localhost' : ((r'(\d+)-(\d+)-(\d+)', 'djangoexample.mailhandler.hooks.handle_important'), 
         (r'(\d+)-(\d+)', 'djangoexample.mailhandler.hooks.handle_junk'))
